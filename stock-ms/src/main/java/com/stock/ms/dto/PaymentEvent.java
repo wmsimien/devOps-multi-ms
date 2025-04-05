4e07a598a5f6662f2f103219a0b77d103f9b48d3
wmavery <wanda.avery@att.net>
2025-04-05 13:11:50 -0500

    add stock service

---
+++ stock-ms/src/main/java/com/stock/ms/dto/PaymentEvent.java
@@ -0,0 +1,28 @@
+package com.stock.ms.dto;
+
+
+public class PaymentEvent {
+
+	private String type;
+
+	private CustomerOrder order;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public CustomerOrder getOrder() {
+		return order;
+	}
+
+	public void setOrder(CustomerOrder order) {
+		this.order = order;
+	}
+	
+	
+
+}
---
+++ stock-ms/src/main/java/com/stock/ms/dto/Stock.java
@@ -0,0 +1,27 @@
+package com.stock.ms.dto;
+
+public class Stock {
+
+	private String item;
+
+	private int quantity;
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+
+}
